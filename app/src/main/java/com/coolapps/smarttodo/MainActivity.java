561d25280878424e0cd052a503a8164d7ac45705
grappeggia <marcosgrappeggia@gmail.com>
2018-05-08 14:13:26 -0700

    Removing hardcoded buttons

--- app/src/main/java/com/coolapps/smarttodo/MainActivity.java
+++ app/src/main/java/com/coolapps/smarttodo/MainActivity.java
@@ -26,15 +26,38 @@ public class MainActivity extends AppCompatActivity {
         //Call the main layout from App XML
         final LinearLayout MainLayoutView = (LinearLayout) findViewById(R.id.mainLayout);
 
-        for (int i=0; i<20; i++){
+        String todoList[] = LoadElements();
+
+        //Draw all UI elements, extracted from a ToDo list
+        for (String todoItem : todoList){
             //Create a view to inflate the main layout
             View tempView = getLayoutInflater().inflate(R.layout.layout_text, MainLayoutView,false);
-
+            TextView tempText = (TextView) tempView.findViewById(R.id.text_item_id);
+            tempText.setText(todoItem);
             //Add the view to the main layout
             MainLayoutView.addView(tempView);
         }
 
 
+    }
+
+    public String[] LoadElements(){
+
+        String todoList[] = {   "1-make todo app",
+                "2-create todo list",
+                "2-create todo list",
+                "2-create todo list",
+                "2-create todo list",
+                "2-create todo list",
+                "2-create todo list",
+                "3-run todo app",
+                "3-run todo app",
+                "3-run todo app",
+                "3-run todo app",
+                "3-run todo app",
+                "3-run todo app"};
+
+        return todoList;
     }
 
 
